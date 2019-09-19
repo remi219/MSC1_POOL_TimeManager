2ed23aba66fb33df71f5c62fad8f2716b043e97e
remi <remi.correnson@epitech.eu>
2019-09-19 09:56:32 +0200

    create routes clocking

--- api/routes/clockings.js
+++ api/routes/clockings.js
@@ -2,12 +2,12 @@ let express = require('express');
 let router = express.Router();
 let clockCtrl = require('../controllers/ClockingController');
 
-router.get('/:id', clockCtrl.getUserById);
+router.get('/:id', clockCtrl.getByUserId);
 
-router.post('/', clockCtrl.createUser);
+router.post('/', clockCtrl.createClock);
 
-/*router.put('/:id', clockCtrl.updateClock);
+router.put('/:id', clockCtrl.updateClock);
 
-router.delete('/:id', clockCtrl.deleteClock);*/
+router.delete('/:id', clockCtrl.deleteClock);
 
 module.exports = router;
