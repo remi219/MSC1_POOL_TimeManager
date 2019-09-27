75f44080e114591a571e1e01895b1cf50550f9b9
remi correnson <remi.correnson@epitech.eu>
2019-09-27 15:40:55 +0200

    try resolve cors pb

--- api/app.js
+++ api/app.js
@@ -20,13 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clocker', clockerRouter);
 app.use('/workingtimes', workingtimesRouter);
-
+app.use('*', cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
