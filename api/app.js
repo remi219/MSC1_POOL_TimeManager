5e367b192554b7a04b8a5f80ab3632d2fa6ca3e2
remi correnson <remi.correnson@epitech.eu>
2019-09-28 19:40:21 +0200

    try fix cors

--- api/app.js
+++ api/app.js
@@ -24,6 +24,12 @@ app.use('/users', usersRouter);
 app.use('/clocker', clockerRouter);
 app.use('/workingtimes', workingtimesRouter);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
