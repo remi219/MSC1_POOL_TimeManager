0ec51dca7975d15a0edc88ad7245841b365b2f09
remi correnson <remi.correnson@epitech.eu>
2019-10-07 15:22:13 +0200

    Update public IP aws

--- front/src/env.js
+++ front/src/env.js
@@ -3,5 +3,5 @@ export default {
     /* Run on localhost */
     //urlHost: 'http://localhost:3001',
     /* Run on AWS instance */
-    urlHost: 'http://54.198.162.120:3001',
+    urlHost: 'http://34.238.172.100:3001',
 };
